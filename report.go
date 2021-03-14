63dc506d94810a9eb9bd6f047a51aa90730beede
Nate <nathanid95@gmail.com>
2021-03-14 03:32:07 -0700

    adding report writing

--- report.go
+++ report.go
@@ -1,6 +1,9 @@
 package traffic
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
 // VehicleMovement is a record of a vehicle's movement in one tick
 type VehicleMovement struct {
@@ -13,16 +16,27 @@ type VehicleMovement struct {
 
 func (m VehicleMovement) String() string {
 	return fmt.Sprintf(
-		"[%d] Vehicle %d/Junction '%s' :: lane '%s' (%s) => '%s' (%s)",
+		"[%d] Vehicle %d in '%s' :: lane '%s' (%s) => '%s' (%s)",
 		m.Tick,
 		m.Vehicle.Identify(),
 		m.InJunction.Identifier,
 		m.FromLane.Identify(),
 		m.FromLane.Direction().String(),
-		m.FromLane.Identify(),
-		m.FromLane.Direction().String(),
+		m.ToLane.Identify(),
+		m.ToLane.Direction().String(),
 	)
 }
 
 // SimulationReport is a sortable, filterable list of vehicle movements in a simulation
 type SimulationReport []VehicleMovement
+
+func (r SimulationReport) Write(writer io.WriteCloser) error {
+	defer writer.Close()
+	for _, line := range r {
+		_, err := writer.Write([]byte(line.String() + "\n"))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
